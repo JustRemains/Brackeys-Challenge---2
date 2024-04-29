0fbc23b0cafdfe6a53e606e153712188051ca7fe
Austin <Austin97Marshall@hotmail.com>
2024-04-29 17:09:45 -0400

    Add project files.

---
+++ Brackeys Challenge - 2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Brackeys_Challenge___2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double num01;
+            double num02;
+
+            Console.Write("Input number here: ");
+
+            num01 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input number here: ");
+
+            num02 = Convert.ToDouble(Console.ReadLine());
+
+            double result = num01 + num02;
+
+            Console.Write(result);
+            
+        }
+    }
+}
